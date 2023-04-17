7e36e535e7895ea8bdae998ae94aa86583eeb39e
CoreSRT <botanicrus@gmail.com>
2023-04-17 22:47:38 +0300

    an update that makes time to freeze (and then refresh unfortunately)

--- main.py
+++ main.py
@@ -11,12 +11,13 @@ class App:
         self.master.resizable(False, False)
         self.master.title('Секундомер')
         self.time_gone = 0  # Начиная с нуля
+        self.tumbler = False  # При запуске программы время на паузе
 
         # Цифра, которая должна показывать сотые:
         self.label = ctk.CTkLabel(self.master, text=str(self.time_gone), font=('Arial bold', 50))
         self.label.place(relx=0.5, rely=0.4, anchor='center')  # 0 to 1
         # Кнопка старта:
-        self.button = ctk.CTkButton(self.master, text='Старт', command=self.start_count, corner_radius=40)
+        self.button = ctk.CTkButton(self.master, text='Старт', command=self.tumbler_switch, corner_radius=40)
         self.button.place(relx=0.5, rely=0.6, anchor='center')
 
     def start_count(self):
@@ -24,9 +25,19 @@ class App:
         t = threading.Thread(target=self.count_time)
         t.start()
 
+    def tumbler_switch(self):
+        t = threading.Thread(target=self.count_time)
+        if self.tumbler:
+            self.tumbler = False
+            print('Тумблер выключен')
+        else:
+            self.tumbler = True
+            print('Тумблер включен')
+            t.start()
+
     def count_time(self):
         start_time = time.time() * 100
-        while True:
+        while self.tumbler:
             try:
                 current_time = time.time() * 100
                 elapsed_time = current_time - start_time
