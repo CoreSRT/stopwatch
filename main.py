87509b468a1eaa3f57e65abb8f581f81fbd99ea2
CoreSRT <botanicrus@gmail.com>
2023-04-18 17:42:45 +0300

    It's fully in English rn.

--- main.py
+++ main.py
@@ -9,15 +9,15 @@ class App:
         self.master = master
         self.master.geometry('350x300')
         self.master.resizable(False, False)
-        self.master.title('Секундомер')
+        self.master.title('Stopwatch')
         self.time_gone = 0  # Starting from zero
-        self.tumbler = False  # При запуске программы время на паузе
+        self.tumbler = False  # Is paused when the program starts
 
         # Цифра, которая должна показывать сотые:
         self.label = ctk.CTkLabel(self.master, text=str(self.time_gone), font=('Arial bold', 50))
         self.label.place(relx=0.5, rely=0.4, anchor='center')  # 0 to 1
         # Кнопка старта:
-        self.button = ctk.CTkButton(self.master, text='Старт', command=self.tumbler_switch, corner_radius=40)
+        self.button = ctk.CTkButton(self.master, text='Start', command=self.tumbler_switch, corner_radius=40)
         self.button.place(relx=0.5, rely=0.6, anchor='center')
 
     def start_count(self):
@@ -43,14 +43,14 @@ class App:
                 current_time = time.time() * 100
                 elapsed_time = current_time - start_time
                 self.time_gone = round(elapsed_time)
-                # Обновляем интерфейс в главном потоке программы
+                # Refreshing the interface at the main window
                 self.master.after(10, self.update_label)
-                time.sleep(0.01)  # ждем 1 сотую секунды
+                time.sleep(0.01)  # waiting a 100'th of second
             except Exception as exc:
                 print(f'Ошибка: {exc}')
 
     def update_label(self):
-        # Обновляем метку на форме
+        # refreshing the lable that's showing time
         self.label.configure(text=str(self.time_gone))
 
 
